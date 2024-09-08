f0e6f2197ccf008508d742666b7b288e7a43ff33
Ayanokoji <104145422+Ayanok0ji@users.noreply.github.com>
2024-09-08 12:40:11 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,8 +1,17 @@
 local Games = {
-    [1782832995] = "https://raw.githubusercontent.com/Ayanok0ji/RvxploitHub/main/Games/AUTPS.lua",
+    [1782832995] = "https://raw.githubusercontent.com/Ayanok0ji/RvxploitHub/main/Games/AUTPS.lua"
 }
-local script = function() 
-    if Games[game.PlaceId] then return game:HttpGet(Games[game.PlaceId]) 
-    else return game:HttpGet(Games[1]) 
-    end 
+
+local script = function()
+    local url = Games[game.PlaceId]
+    
+    if url then 
+        return game:HttpGet(url)
+    else 
+        return "No script available for this game."
+    end
 end
+
+-- Usage
+local scriptContent = script()
+print(scriptContent)
