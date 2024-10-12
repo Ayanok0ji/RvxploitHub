82efed501d88046eb712d7d866e56ff44501c16e
Ayanokoji <104145422+Ayanok0ji@users.noreply.github.com>
2024-10-12 15:47:11 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -8,7 +8,15 @@ local Supported = {
 }
 
 if table.find(Supported.A_Universal_Time,game.PlaceId) then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lvl9999/FlamesW/new/AUT"))();
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Ayanok0ji/RvxploitHub/main/Games/AUTPS.lua"))();
 elseif table.find(Supported.Jjmon,game.PlaceId) then
     loadstring(game:HttpGet(""))();
 end
+
+-- Anti AFK
+local afk? = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+   afk?:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+   wait(1)
+   afk?:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
