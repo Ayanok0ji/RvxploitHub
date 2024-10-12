63eb3cb46a4067563bc84b96c44eeaea0ab2ee5e
Ayanokoji <104145422+Ayanok0ji@users.noreply.github.com>
2024-10-12 15:45:09 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,14 +1,14 @@
 repeat task.wait(0.25) until game:IsLoaded();
-getgenv().CurrentVersion = "1.05";
+getgenv().CurrentVersion = "1";
 
 local Supported = {
-    A_Universal_Time = {6846458508,5130598377};
-    Heaven_Stand = {14561998168};
+    A_Universal_Time = {5130598377};
+    Jjmon = {14561998168};
 
 }
 
 if table.find(Supported.A_Universal_Time,game.PlaceId) then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lvl9999/FlamesW/new/AUT"))();
-elseif table.find(Supported.Heaven_Stand,game.PlaceId) then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lvl9999/FlamesW/new/HS"))();
+elseif table.find(Supported.Jjmon,game.PlaceId) then
+    loadstring(game:HttpGet(""))();
 end
